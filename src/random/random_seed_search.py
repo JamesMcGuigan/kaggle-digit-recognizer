694724063e0656e459851333eb3f5c8cb369f558
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-03-01 20:19:58 +0000

    random | random_seed_search.py | add quotation for Kaggle

--- src/random/random_seed_search.py
+++ src/random/random_seed_search.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # Source: https://www.kaggle.com/jamesmcguigan/minst-random-seed-search
+"""
+"We've heard that a million monkeys at a million keyboards could produce the complete works of Shakespeare;
+now, thanks to the Internet, we know that is not true."
+- Robert Wilensky
+"""
 
 import tensorflow as tf
 import pandas as pd
@@ -22,13 +27,13 @@ best_guess  = None     # tf.random.uniform((answers_size,), minval=0, maxval=9, 
 best_count  = 0        # tf.math.count_nonzero(tf.equal(answers, best_guess)).numpy()
 
 min_seed    = max(0, best_seed)
-max_seed    = sys.maxsize  # = 350 million years
-max_seed    = 100000000    # = 30h
-max_seed    = 10000000     # = 3h
-max_seed    = 1000000      # = 16m
-max_seed    = 100000       # = 2m   # Best under 100000 | seed = 25815 | count = 3074 | accuracy = 0.1098
-# max_seed  = 10000        # = 10s  # Best under 10000  | seed = 9322  | count = 3042 | accuracy = 0.1086
-# max_seed  = 1000         # = 1s   # Best under 1000   | seed = 558   | count = 2964 | accuracy = 0.1059
+max_seed    = sys.maxsize            # = 350 million years
+max_seed    = 100000000              # = 30h
+max_seed    = int(1000*60*60*6/1.5)  # = Kaggle 6 hour script timeout @ 1.5ms/unit
+# max_seed  = 10000000               # = 3h
+# max_seed  = 1000000                # = 16m  # Best under 100000   | seed = 25815   | count = 3074 | accuracy = 0.1098
+# max_seed  = 100000                 # = 2m   # Best under 10000    | seed = 9322    | count = 3042 | accuracy = 0.1086
+# max_seed  = 1000                   # = 1s   # Best under 1000     | seed = 558     | count = 2964 | accuracy = 0.1059
 
 
 ### Delete submissions: [ os.remove(file) for file in os.listdir() if file.endswith('.csv') ]
@@ -40,8 +45,8 @@ def submission(max_seed, best_seed, best_guess):
         })
     print('-----')
     print(f"Best under {max_seed} | seed = {best_seed} | count = {best_count} | accuracy = {round(best_count / answers_size,4)}")
-    submission.to_csv(f'{log_dir}/submission-max={max_seed}-seed={best_seed}-count={best_count}.csv', index=False)
-    print("Wrote:",   f'{log_dir}/submission-max={max_seed}-seed={best_seed}-count={best_count}.csv', best_guess.shape)
+    submission.to_csv(f'{log_dir}/max={max_seed}-seed={best_seed}-count={best_count}-submission.csv', index=False)
+    print("Wrote:",   f'{log_dir}/max={max_seed}-seed={best_seed}-count={best_count}-submission.csv', best_guess.shape)
     print('-----')
 
 
@@ -50,7 +55,8 @@ print_seed  = 1000
 while min_seed >= print_seed: print_seed *= 10
 
 for seed in range(min_seed, max_seed):
-    tf.random.set_seed(seed)  # tf.random.set_seed(seed) is deterministic | tf.random.uniform(seed=seed) is not (depends on gobal seed)
+    # NOTE: tf.random.set_seed(seed) is deterministic | tf.random.uniform(seed=seed) is not (depends on gobal seed)
+    tf.random.set_seed(seed)
     guess = tf.random.uniform((answers_size,), minval=0, maxval=9, dtype=tf.dtypes.int32)
     count = tf.math.count_nonzero(tf.equal(answers, guess)).numpy()
     if count > best_count:
