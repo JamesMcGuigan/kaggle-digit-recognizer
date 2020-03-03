b9294fac9b03dd36d6fadf41474fe7dfd330b4e4
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-03-03 12:42:36 +0000

    random_seed_search | update code from kaggle

--- src/random/random_seed_search.py
+++ src/random/random_seed_search.py
@@ -6,11 +6,13 @@ now, thanks to the Internet, we know that is not true."
 - Robert Wilensky
 """
 
-import tensorflow as tf
-import pandas as pd
-import time
-import sys
 import os
+
+import humanize
+import pandas as pd
+import tensorflow as tf
+import time
+
 os.environ['TF_DETERMINISTIC_OPS'] = '1'
 
 data_dir = './data'
@@ -22,39 +24,51 @@ answers_csv  = pd.read_csv(f'{data_dir}/answers.csv', comment='#')
 answers_size = answers_csv.shape[0]
 answers      = tf.constant(answers_csv['Label'].loc[0:answers_size - 1], dtype=tf.dtypes.int32)
 
-best_seed   = 0        # CPU
+found_seeds = [ 558, 9322, 25815, 11328613, 45576972 ]
+best_seed   = 45576972 # CPU
 best_guess  = None     # tf.random.uniform((answers_size,), minval=0, maxval=9, dtype=tf.dtypes.int32, seed=best_seed)
 best_count  = 0        # tf.math.count_nonzero(tf.equal(answers, best_guess)).numpy()
 
+kaggle_time_limit = int(1000*60*60*9/1.5)  # = Kaggle 9 hour script timeout @ 1.5ms/unit
 min_seed    = max(0, best_seed)
-max_seed    = sys.maxsize            # = 350 million years
-max_seed    = 100000000              # = 30h
-max_seed    = int(1000*60*60*6/1.5)  # = Kaggle 6 hour script timeout @ 1.5ms/unit
+# max_seed  = sys.maxsize            # = 350 million years
+# max_seed  = 100000000              # = 30h
 # max_seed  = 10000000               # = 3h
 # max_seed  = 1000000                # = 16m  # Best under 100000   | seed = 25815   | count = 3074 | accuracy = 0.1098
 # max_seed  = 100000                 # = 2m   # Best under 10000    | seed = 9322    | count = 3042 | accuracy = 0.1086
 # max_seed  = 1000                   # = 1s   # Best under 1000     | seed = 558     | count = 2964 | accuracy = 0.1059
 
+min_seed    = 50000000               # Kaggle Kernel v6 Search Limit
+max_seed    = max(min_seed, best_seed) + kaggle_time_limit
+max_seed    = int(round(max_seed, -(len(str(max_seed))-3)))  # round max_seed to 2sf
 
 ### Delete submissions: [ os.remove(file) for file in os.listdir() if file.endswith('.csv') ]
-def submission(max_seed, best_seed, best_guess):
+def submission(max_seed, best_seed, best_guess, best_count):
     best_count = tf.math.count_nonzero(tf.equal(answers, best_guess)).numpy()
     submission = pd.DataFrame({
         "ImageId":  range(1, 1+best_guess.shape[0]),
         "Label":    best_guess
-        })
+    })
     print('-----')
-    print(f"Best under {max_seed} | seed = {best_seed} | count = {best_count} | accuracy = {round(best_count / answers_size,4)}")
+    print(f"Best under {humanize.intcomma(max_seed)} | seed = {best_seed} | count = {best_count} | accuracy = {round(best_count / answers_size,4)}")
     submission.to_csv(f'{log_dir}/max={max_seed}-seed={best_seed}-count={best_count}-submission.csv', index=False)
     print("Wrote:",   f'{log_dir}/max={max_seed}-seed={best_seed}-count={best_count}-submission.csv', best_guess.shape)
     print('-----')
 
 
-timer_start = time.time()
-print_seed  = 1000
-while min_seed >= print_seed: print_seed *= 10
+print_seed = 1000
+def increment_print_seed():
+    global print_seed
+    if print_seed >= 10000000:  print_seed += 10000000  # = 4 hours
+    else:                       print_seed *= 10
 
-for seed in range(min_seed, max_seed):
+
+def test_seed(seed):
+    global best_seed
+    global best_count
+    global best_guess
+    global print_seed
+
     # NOTE: tf.random.set_seed(seed) is deterministic | tf.random.uniform(seed=seed) is not (depends on gobal seed)
     tf.random.set_seed(seed)
     guess = tf.random.uniform((answers_size,), minval=0, maxval=9, dtype=tf.dtypes.int32)
@@ -65,10 +79,26 @@ for seed in range(min_seed, max_seed):
         best_count = count
         best_guess = guess
     if seed == print_seed:
-        submission(print_seed, best_seed, best_guess)
-        print_seed *= 10
+        submission(print_seed, best_seed, best_guess, best_count)
+        increment_print_seed()
 
-submission(max_seed, best_seed, best_guess)
-time_taken = time.time() - timer_start
-time_unit  = time_taken / (max_seed-min_seed)
-print(f'Finished in {round(time_taken,2)}s - {round(time_unit*1000,2)}ms/unit')  # 1.21ms/unit CPU | 1.32ms/unit GPU | 1.19/ms TPU
+
+
+if __name__ == "__main__":
+    print(f'-----')
+    print(f'Random Seed Search: {humanize.intcomma(min_seed)} -> {humanize.intcomma(max_seed)}')
+    print(f'-----')
+    timer_start = time.time()
+
+    for seed in found_seeds:
+        while print_seed < seed: increment_print_seed()
+        test_seed(seed)
+        test_seed(print_seed)  # == submission(seed)
+
+    for seed in range(min_seed, max_seed):
+        test_seed(seed)
+
+    submission(max_seed, best_seed, best_guess, best_count)
+    time_taken = time.time() - timer_start
+    time_unit  = time_taken / (max_seed-min_seed)
+    print(f'Finished in {round(time_taken,2)}s - {round(time_unit*1000,2)}ms/unit')  # 1.21ms/unit CPU | 1.32ms/unit GPU | 1.19/ms TPU
